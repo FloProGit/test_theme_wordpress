19f27382dbfaebdab63f6ba67d76add200df8a6b
florian <sudreflorianpro@gmail.com>
2023-07-17 16:02:38 +0100

    feat:add tailwind css

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 namespace FSTheme;
 function theme_supports (){
     add_theme_support('title-tag');
+    wp_enqueue_style('tailwindcss-output',get_template_directory_uri().'/dist/output.css');
 }
 
 add_action( 'after_setup_theme' ,'FSTheme\theme_supports');
